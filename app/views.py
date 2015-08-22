4afecdaaa80dca2f7a15373f9cadce719dbecb82
seansaito <saitosean@ymail.com>
2015-08-22 12:49:48 +0800

    More defensive

--- app/views.py
+++ app/views.py
@@ -10,6 +10,10 @@ def form():
     result = request.form.getlist("1")
     dog=4
     cat=0
+
+    if len(result) != 2:
+        return render_template("index.html")
+    
     importance, user_choice = result
 
     dog_diff = int(importance) * abs(dog - int(user_choice))
