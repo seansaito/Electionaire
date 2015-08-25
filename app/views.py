82225b4a1b6d5ecd41767f0bf3be8b0ef575410a
seansaito <saitosean@ymail.com>
2015-08-26 01:21:58 +0800

    Changes to routes and stuff

--- app/views.py
+++ app/views.py
@@ -3,29 +3,29 @@ from flask import render_template, redirect, url_for, request
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        result = request.form.getlist("1")
+        dog=4
+        cat=0
+        if len(result) != 2:
+            return render_template("index.html")
 
-@app.route("/form", methods=["POST"])
-def form():
-    result = request.form.getlist("1")
-    dog=4
-    cat=0
+        importance, user_choice = result
 
-    if len(result) != 2:
-        return render_template("index.html")
-    
-    importance, user_choice = result
+        dog_diff = int(importance) * abs(dog - int(user_choice))
+        cat_diff = int(importance) * abs(cat - int(user_choice))
 
-    dog_diff = int(importance) * abs(dog - int(user_choice))
-    cat_diff = int(importance) * abs(cat - int(user_choice))
+        thing = ""
 
-    thing = ""
+        if dog_diff > cat_diff:
+            thing = "cat"
+        elif cat_diff > dog_diff:
+            thing = "dog"
+        else:
+            thing = "human"
 
-    if dog_diff > cat_diff:
-        thing = "cat"
-    elif cat_diff > dog_diff:
-        thing = "dog"
+        print request.remote_addr
+
+        return render_template("answer.html", thing=thing, ip=request.remote_addr)
     else:
-        thing = "human"
-
-    return render_template("answer.html", thing=thing)
+        return render_template("index.html")
