7b718fcdd3fe5a6f2d484c14cd53f73006cfecb3
seansaito <saitosean@ymail.com>
2015-09-01 21:49:05 +0800

    Finalized questions and options

--- app/controllers/candidate_match.py
+++ app/controllers/candidate_match.py
@@ -24,16 +24,16 @@ class CandidateMatcher():
 
     def __init__(self, num_questions):
         self.candidates = {
-            "PAP": Candidate([(1, 4), (2, 1), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (13, 0)], "PAP (People's Action Party)", "PAP"),
-            "SG1ST": Candidate([(1, 0), (2, 4), (3, 4), (4,4), (5,4), (6, 3), (7, 0), (13, 2)], "SG1ST (Singaporeans First)", "SG1ST"),
-            "SDA": Candidate([(1, 2), (2, 4), (3, 3), (4, 4), (5, ""), (6, ""), (7, 4), (13, 4)], "SDA (Singapore Democratic Alliance)", "SDA"),
-            "WP": Candidate([(1, 2), (2, 3), (3, 2), (4, 3), (5, 0), (6, 4), (7, 4), (13, 4)], "WP (Worker's Party)", "WP"),
-            "SDP": Candidate([(1, 2), (2, 2), (3, 3), (4,4), (5, 4), (6, 4), (7, 4), (13, 4)], "SDP (Singapore Democratic Party)", "SDP"),
-            "DPP": Candidate([(1, 2), (2, 2), (3, 3), (4, 4), (5, 3), (6, ""), (7, 3), (13, 1)], "DPP (Democratic Progressive Party)", "DPP"),
-            "NSP": Candidate([(1, 2), (2, 1), (3, 2), (4, 2), (5, 2), (6, 4), (7, ""), (13, "")], "NSP (National Solidarity Party)", "NSP"),
-            "RP": Candidate([(1, 0), (2, 4), (3, 2), (4, 3), (5, 4), (6, 4), (7, ""), (13, 4)], "RP (Reform Party)", "RP"),
-            "SPP": Candidate([(1, 2), (2, 2), (3, 1), (4, 3), (5, 0), (6, 4), (7, 4), (13, 4)], "SPP (Singapore People's Party)", "SPP"),
-            "PPP": Candidate([(1, 2), (2, 2), (3, 2), (4, 3), (5, 0), (6, 4), (7, ""), (13, "")], "PPP (People's Power Party)", "PPP")
+            "PAP": Candidate([(1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 1), (8, 0), (9, 0), (10, 0), (11, 2), (12, 1), (13, 0), (14, 0), (15, 0)], "PAP (People's Action Party)", "PAP"),
+            "SG1ST": Candidate([(1, 4), (2, 4), (3, 4), (4, 4), (5, 4), (6, 3), (7, 2), (8, 4), (9, 3), (10, 3), (11, 3), (12, 2), (13, 3), (14, ""), (15, 3)], "SG1ST (Singaporeans First)", "SG1ST"),
+            "SDA": Candidate([(1, 3), (2, 4), (3, 4), (4, 4), (5, ""), (6, 0), (7, 4), (8, 4), (9, 4), (10, 4), (11, 4), (12, ""), (13, 3), (14, ""), (15, "")], "SDA (Singapore Democratic Alliance)", "SDA"),
+            "WP": Candidate([(1, 4), (2, 3), (3, 3), (4, 3), (5, 2), (6, 3), (7, 4), (8, 3), (9, 4), (10, 4), (11, 3), (12, 2), (13, 2), (14, 3), (15, 4)], "WP (Worker's Party)", "WP"),
+            "SDP": Candidate([(1, 3), (2, 2), (3, 4), (4, 4), (5, 4), (6, 4), (7, 4), (8, 4), (9, 4), (10, 4), (11, 4), (12, 4), (13, 4), (14, 4), (15, 4)], "SDP (Singapore Democratic Party)", "SDP"),
+            "DPP": Candidate([(1, 3), (2, 3), (3, 3), (4, 3), (5, 3), (6, 3), (7, 3), (8, 2), (9, 2), (10, 3), (11, 4), (12, 2), (13, 3), (14, ""), (15, 3)], "DPP (Democratic Progressive Party)", "DPP"),
+            "NSP": Candidate([(1, 4), (2, 4), (3, 3), (4, 4), (5, 2), (6, 4), (7, 1), (8, 3), (9, 3), (10, 4), (11, 3), (12, 3), (13, 3), (14, ""), (15, 3)], "NSP (National Solidarity Party)", "NSP"),
+            "RP": Candidate([(1, 2), (2, 4), (3, 2), (4, 3), (5, 4), (6, 4), (7, 3), (8, 4), (9, 4), (10, 3), (11, 4), (12, 3), (13, 4), (14, ""), (15, 4)], "RP (Reform Party)", "RP"),
+            "SPP": Candidate([(1, 4), (2, 2), (3, 3), (4, 2), (5, ""), (6, 3), (7, 3), (8, 3), (9, 3), (10, 3), (11, 2), (12, 4), (13, 4), (14, ""), (15, 3)], "SPP (Singapore People's Party)", "SPP"),
+            "PPP": Candidate([(1, 2), (2, 4), (3, 2), (4, 3), (5, ""), (6, 3), (7, ""), (8, ""), (9, ""), (10, 4), (11, 2), (12, ""), (13, 3), (14, ""), (15, 2)], "PPP (People's Power Party)", "PPP")
         }
         self.num_questions = num_questions
 
